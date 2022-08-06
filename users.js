43c0f0ca9b38aa455bb9b932651445cc55398f17
Prathamesh Thorat <prathameshthorat625@gmail.com>
2022-08-06 10:42:04 +0530

    make it better

--- users.js
+++ users.js
@@ -88,6 +88,8 @@ router.post("/emailUpdation", async (req, res) => {
   res.json(update);
 });
 
+/*
+
 router.post("/purchased", async (req, res) => {
   const purchased = await userSchema.findByIdAndUpdate(
     { _id: req.body._id },
@@ -100,6 +102,7 @@ router.post("/purchased", async (req, res) => {
   res.json(purchased);
 });
 
+*/
 router.post("/login", async (req, res) => {
   const userLogin = await userSchema.findOne({
     email: req.body.email,
