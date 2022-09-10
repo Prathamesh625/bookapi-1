4a4d0033952516e3daa2adbe11b2f765d24f4303
Prathamesh625 <92034832+Prathamesh625@users.noreply.github.com>
2022-09-10 13:56:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 //mongo = "mongodb://localhost/database";
 
