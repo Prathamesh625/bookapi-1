8da9e0994db53e1ec3d49039ad0bcc47ca2a6527
Prathamesh Thorat <prathameshthorat625@gmail.com>
2022-09-10 10:45:48 +0530

    make it better

--- server.js
+++ server.js
@@ -5,11 +5,22 @@ const mongoose = require("mongoose");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+/*
 app.use(
   cors({
     origin: "*",
   })
 );
+
+*/
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 //mongo = "mongodb://localhost/database";
 
